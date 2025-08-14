ec042b714370f8ef227af22f5fd02c90f34b80a6
hamzamekk <hamza.mekkoudi@gmail.com>
2025-08-14 19:46:16 +0100

    feat: initialize project with yarn and cocoapods configuration, add turboPreferences module

--- src/NativeTurboPreferences.ts
+++ src/NativeTurboPreferences.ts
@@ -3,6 +3,9 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
+  setName(name: string): void;
+  get(key: string): Promise<string | null>;
+  set(key: string, value: string): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('TurboPreferences');
